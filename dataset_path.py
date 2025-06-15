769db088d37d3e0a98170f6f025b596560ef04fc
Xinlong (Luke) Zhao <95112515+GeekEagle@users.noreply.github.com>
2025-06-14 19:30:19 -0700

    Update dataset_path.py

--- dataset_path.py
+++ dataset_path.py
@@ -2,11 +2,9 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'SimGas':
-            # return './datasets/kfold_SimGas/'
-            return '/home/geek1234/datasets/kfold_marshall/'
+            return './datasets/kfold_SimGas/'
         elif dataset == 'IGS-Few':
-            # return './datasets/IGS-Few/'
-            return '/home/geek1234/datasets/IGS-Few/'
+            return './datasets/IGS-Few/'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
